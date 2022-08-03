5b34ec9969f14924571b8ec4b5d2622ecebf3435
managment_system <mas152q@gmail.com>
2022-08-03 15:31:16 +1000

    header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
+import Navbar from '../Nav/Navbar';
 
 const Header = () => {
   return (
@@ -8,6 +9,7 @@ const Header = () => {
       <Link to="/">
         <h1>Prism-Breaker</h1>
       </Link>
+      <Navbar />
     </header>
   );
 };
