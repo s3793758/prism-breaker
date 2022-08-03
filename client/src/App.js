50ffc81cd644087907b3d301361919a79597cd83
managment_system <mas152q@gmail.com>
2022-08-03 15:32:22 +1000

    app with nav, header, footer componenets

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,44 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
 function App() {
-  const [user, setUser] = useState(null);
-  if (user) {
-    setUser(user);
-  }
+  const [user, setUser] = useState(() => {
+    try {
+      const user = JSON.parse(localStorage.getItem('user')) || null;
+      return user;
+    } catch (error) {
+      return null;
+    }
+  });
+
+  console.log({ user });
+
+  const isLoggedIn = !!user?._id;
+  console.count('rendered');
+
+  const updateUser = (user) => {
+    if (user) {
+      setUser(user);
+    }
+  };
+
+  const logoutUser = async () => {
+    try {
+      localStorage.removeItem('user');
+      setUser(null);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
   return (
     <div className="App">
       <div className="container">
         <BrowserRouter>
           <Header />
-          <AuthContext.Provider value={{ user, setUser }}>
+          <AuthContext.Provider
+            value={{ user, updateUser, isLoggedIn, logoutUser }}
+          >
             <Routes>
               <Route path="/" element={<Navigate to="/login" />} />
               <Route path="/login" element={<SignIn />} />
